256c600a7388ebd13745f4d5bbb5b586ba417f31
Kochelaev <qrbox@mail.ru>
2022-10-17 21:26:06 +0400

    Category edit form

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -28,6 +28,8 @@ Route::group(['prefix' => 'admin'], function () {
         Route::get('/create', Controllers\Admin\Category\CreateController::class)->name('admin.category.create');
         Route::post('/', Controllers\Admin\Category\StoreController::class)->name('admin.category.store');
         Route::get('/{category}', Controllers\Admin\Category\ShowController::class)->name('admin.category.show');
+        Route::get('/{category}/edit', Controllers\Admin\Category\EditController::class)->name('admin.category.edit');
+
     });
 });
 
